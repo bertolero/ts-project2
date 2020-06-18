d3954e5b1c37b2e73d19674bb36bd9d3c4ced618
Bertol <bertol.pinheiro@gmail.com>
2020-06-18 10:25:48 -0300

    Add basic form interaction.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+function searchAddressHandler (event: Event) {
+  event.preventDefault()
+  const addressEntered = addressInput.value
+}
+
+form.addEventListener('submit', searchAddressHandler)
